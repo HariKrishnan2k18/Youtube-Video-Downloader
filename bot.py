1982f0e797463a2285c1629f7d79b6d6bb4fe782
Harikrishnan <84627623+HariKrishnan2k18@users.noreply.github.com>
2024-09-28 22:37:32 +0530

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | LISA-KOREA/YouTube-Video-Download-Bot
+
+# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/YouTube-Video-Download-Bot
+
+
+
+from pyrogram import Client, filters
+# from Youtube.config import Config
+
+# Create a Pyrogram client
+app = Client(
+    "my_bot",
+    api_id='19340501', 
+    api_hash='7dd7b1984a85c0258c87c17cfac1542b', 
+    bot_token='7624800788:AAE0raDqJZMX5KPDAsmjN2uDTZb4sNadbhk',
+    plugins=dict(root="Youtube")
+)
+
+
+
+# Start the bot
+print("🎊 I AM ALIVE 🎊")
+app.run()
